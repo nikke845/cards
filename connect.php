d29130e8ebc0c72f1174b894e037363ce4e626b4
nikke845 <niklas.vuorivirta@myy.haaga-helia.fi>
2019-02-14 12:34:56 +0200

    muutettiin connect.php:n koodia

--- connect.php
+++ connect.php
@@ -5,8 +5,6 @@ function connect_db() {
  $password = "jaQYNe68d";
  $dbname = "a1704502";
  // Create connection
-GIT Exercises 4 (13)
-29.12.2018
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
  if ($conn->connect_error) {
